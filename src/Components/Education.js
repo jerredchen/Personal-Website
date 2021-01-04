d892d15d90d326e09b33f75c9b386a3bb1b3c80f
jerredchen <jchen788@gatech.edu>
2021-01-04 16:59:44 -0600

    Redesigned education for mobile

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -13,12 +13,15 @@ function Education() {
             <div className="education-block">
                 <div className="row-item">
                     <div className="education-title">
-                        <h1><span>Education</span></h1>
+                        <h1>Education</h1>
+                    </div>
+                    <div className="college-name">
+                        <h3>Georgia Institute of Technology</h3>
+                    </div>
+                    <div className="date-container">
+                        <span className="date">Expected Fall 2022</span>
                     </div>
                     <div className="college-info">
-                        <h3>Georgia Institute of Technology
-                            <span>Expected Fall 2022</span>
-                        </h3>
                         <p>Bachelor's in Mechanical Engineering</p>
                         <p>Bachelor's in Computer Science<br/>- Concentrations in Artificial Intelligence, Modeling & Simulation</p>
                         <p>GPA: 3.88</p>
@@ -26,25 +29,27 @@ function Education() {
                 </div>
             </div>
 
-            <div className="coursework">
+            <div className="row-item">
                 <h1>Relevant<br/>Coursework</h1>
-                <div id="course-top" className="course-block">Completed:</div>
-                <ul className="course-block">
-                    <li>ECE 3170: Circuits and Electronics</li>
-                    <li>MATH 2552: Differential Equations</li>
-                    <li>MATH 3670: Statistics</li>
-                    <li>ME 3017: System Dynamics</li>
-                    <li>CS 1332: Data Structures and Algorithms</li>
-                    <li>CS 2050: Discrete Mathematics</li>
-                    <li>CS 2110: Computer Organization and Programming</li>
-                    <li>CS 3600: Introduction to Artificial Intelligence</li>
-                </ul>
-                <div className="course-block">In Progress:</div>
-                <ul className="course-block">
-                    <li>CS 2200: Systems and Networks</li>
-                    <li>CS 3510: Design Analysis of Algorithms</li>
-                    <li>CS 3630: Robotics and Perception</li>
-                </ul>
+                {/* <div id="course-top" className="course-block">Completed:</div> */}
+                <div className="course-block">
+                    <p className="course-top">Completed:</p>
+                    <p>ECE 3170: Circuits and Electronics</p>
+                    <p>MATH 2552: Differential Equations</p>
+                    <p>MATH 3670: Statistics</p>
+                    <p>ME 3017: System Dynamics</p>
+                    <p>CS 1332: Data Structures and Algorithms</p>
+                    <p>CS 2050: Discrete Mathematics</p>
+                    <p>CS 2110: Computer Organization and Programming</p>
+                    <p>CS 3600: Introduction to Artificial Intelligence</p>
+                </div>
+                {/* <div className="course-block">In Progress:</div> */}
+                <div className="course-block">
+                    <p className="course-top">In Progress:</p>
+                    <p>CS 2200: Systems and Networks</p>
+                    <p>CS 3510: Design Analysis of Algorithms</p>
+                    <p>CS 3630: Robotics and Perception</p>
+                </div>
             </div>
         </div>
         </section>
